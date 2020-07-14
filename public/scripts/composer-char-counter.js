ac333f5c18314cf3fa4ff9ec3ed02a724eb67de8
sE7rgo <se7rgo@gmail.com>
2020-07-14 23:47:29 +0000

    hover shadow and counter is red when negative

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,12 +1,14 @@
 $(document).ready(function() {
   let num = 140;
 
-  $('.new-tweet .counter').val(num);
+  $('.new-tweet div output').val(num);
 
   $('#tweet-text').keypress(function () {
     num--;
-    $(this).siblings('div').children('.counter').val(num);
     if (num < 0) {
+      $(this).siblings('div').children('output').val(num).css('color', 'red')
+    } else {
+      $(this).siblings('div').children('output').val(num)
     }
   });
 });
