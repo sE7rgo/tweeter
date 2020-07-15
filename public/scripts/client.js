d846fb6b43564a95e0ed4ee0eccd3213905ba06b
sE7rgo <se7rgo@gmail.com>
2020-07-15 21:57:04 +0000

    renderTweets and createTweets working state

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,48 +4,78 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
       "handle": "@SirIsaac"
     },
-  "content": {
+    "content": {
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
-  "created_at": 1461116232227
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+const timeDif = function (time) {
+  const diff = (Date.parse(new Date) - time)
+  const sec = diff / 1000;
+  const min = sec / 60;
+  const hours = min / 60;
+  const days = hours / 24;
+  const years = days / 365;
+  if (years > 0) {
+    return `${Math.floor(years)} years ago`;
+  } else if (years < 0 && days > 0) {
+    return `${Math.floor(days)} days ago`;
+  } else {
+    return `${Math.floor(hours)} hours ago`;
+  }
 }
 
 $(document).ready(() => {
-  console.log('ready')
-  
-  const createTweetElement = function(data) {
-    $('.tweet header div h4').text(data.user.name);
-    $('.tweet header div img').attr('src', data.user.avatars);
-    $('.tweet header div.handle').text(data.user.handle);
-    $('.tweet .content').text(data.content.text);
-
-    const timeDif = function (time) {
-      const diff = (Date.parse(new Date) - time)
-      const sec = diff / 1000;
-      const min = sec / 60;
-      const hours = min / 60;
-      const days = hours / 24;
-      const years = days / 365;
-      if (years > 0) {
-        return `${Math.floor(years)} years ago`;
-      } else if (years < 0 && days > 0) {
-        return `${Math.floor(days)} days ago`;
-      } else {
-        return `${Math.floor(hours)} hours ago`;
-      }
+  const renderTweets = function(tweets) {
+    for (const tweet of tweets) {
+      $('.container').append(createTweetElement(tweet));
     }
-    $('.tweet footer div.time').text(timeDif(data.created_at))
+  };
+  const createTweetElement = function(data) {
+    const $tweet = `<article class="tweet">
+    <header>
+      <div>
+        <img src="${data.user.avatars}">
+        <br>
+        <h4>${data.user.name}</h4>
+      </div>
+      <div>
+        <h4></h4>>${data.user.handle}</h4>
+      </div>
+    </header>
+    <section>
+    <p>${data.content.text}</p>
+    </section>
+    <footer>
+      <div>
+        <h6>${timeDif(data.created_at)}</h6>
+      </div>
+      <div>
+        🍺☺️👾 🤖
+      </div>
+    </footer>
+  </article>`
+  return $tweet;
   }
-  const $tweet = createTweetElement(tweetData);
-  //get tweetData
-  // Test / driver code (temporary)
-  console.log($tweet); // to see what it looks like
-  $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+renderTweets(data);
 })
-
